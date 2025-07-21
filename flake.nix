004c0e5f7f7fb91beed94646016b7c150c955355
Hannes Fahlin <hannes.fahlin@gmail.com>
2025-07-21 15:23:32 +0200

    Flake system config

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "System Config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }: 
+  let
+    system = "x86-64-linux";
+    
+    pkgs = import nixpkgs {
+      inherit system;
+      config = { allowUnfree = true; };
+    };
+
+    lib = nixpkgs.lib;
+
+  in {
+    nixosConfigurations = {
+      nixos = lib.nixosSystem {
+        inherit system;
+
+	modules = [
+	  ./system/configuration.nix
+	];
+      };
+    };
+  };
+}
